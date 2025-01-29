f35e85d4ed7edc2bfea0741ea766f3a264a93418
Niraj Kumar <131793238+niraj1703@users.noreply.github.com>
2025-01-29 14:19:19 +0000

    variable

---
+++ print.java
@@ -0,0 +1,22 @@
+public class print {
+    public static void main(String[] args) {
+        System.out.println ("Hello, World!");
+        System.out.println("Hi!");
+        System.out.print("nice to meert you ");
+        System.out.print("my name is niraj and");
+        System.out.print(" I am learning Java.");
+        System.out.print("It is awesome!");
+        System.out.print(" This sentence will work!");
+        System.out.print("I will print on the same line.");
+        System.out.println("ok ");
+        System.out.println(3);
+        System.out.println(358);
+        System.out.println(50000);
+        System.out.print(3 );
+        System.out.println(58 );
+        System.out.println(500);
+        System.out.println(3 + 3);
+        System.out.println(2 * 5);
+    }
+  
+}
---
+++ variable.java
@@ -0,0 +1,58 @@
+public class variable {
+    public static void main(String[] args) {
+        // String name = "niraj";
+        // int age = 25;
+        // float height = 5.99f;
+        // height = 6f;
+        // char grade = 'd';
+        // grade = 'a';
+        // boolean isStudent = true;
+        // isStudent = false;
+        // System.out.println(name);
+        // System.out.println("Name: " + name + ", Age: " + age + ", Height: " + height +
+        //         ", Grade: " + grade + ", Is Student: " + isStudent);
+
+        // String name = "John";
+        // System.out.println("Hello " + name);  //print variable
+
+
+        // String firstName = "John ";
+        // String lastName = "Doe";
+        // String fullName = firstName + lastName;
+        // System.out.println(fullName);
+        // int myNum = 15;
+        // myNum = 10;
+        // System.out.println(myNum);
+
+        // final int myNum = 15;
+        // myNum = 20; // will generate an error
+        // System.out.println(myNum);
+
+//         int myNum;
+// myNum = 1000;
+// System.out.println(myNum);
+
+// int myNum = 5;
+// float myFloatNum = 5.99f;
+// char myLetter = 'D';
+// boolean myBool = true;
+// String myText = "Hello";
+
+
+int x = 5;
+int y = 6;
+int z = 50;
+System.out.println(x + y + z);
+
+int a, b, c;
+a = b = c = 50;
+System.out.println(a + b + c);
+
+    }
+}
+
+
+
+
+
+
