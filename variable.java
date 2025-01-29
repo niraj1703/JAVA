60c15b0aeb1e7c616f9320583f367c4ebaf2179b
Niraj Kumar <131793238+niraj1703@users.noreply.github.com>
2025-01-29 14:28:27 +0000

    commite

--- variable.java
+++ variable.java
@@ -1,44 +1,47 @@
 public class variable {
     public static void main(String[] args) {
-        // String name = "niraj";
-        // int age = 25;
-        // float height = 5.99f;
-        // height = 6f;
-        // char grade = 'd';
-        // grade = 'a';
-        // boolean isStudent = true;
-        // isStudent = false;
-        // System.out.println(name);
-        // System.out.println("Name: " + name + ", Age: " + age + ", Height: " + height +
-        //         ", Grade: " + grade + ", Is Student: " + isStudent);
+        String name = "niraj";
+        int age = 25;
+        float height = 5.99f;
+        height = 6f;
+        char grade = 'd';
+        grade = 'a';
+        boolean isStudent = true;
+        isStudent = false;
+        System.out.println(name);
+        System.out.println("Name: " + name + ", Age: " + age + ", Height: " + height +
+                ", Grade: " + grade + ", Is Student: " + isStudent);
 
-        // String name = "John";
-        // System.out.println("Hello " + name);  //print variable
-
-
-        // String firstName = "John ";
-        // String lastName = "Doe";
-        // String fullName = firstName + lastName;
-        // System.out.println(fullName);
-        // int myNum = 15;
-        // myNum = 10;
-        // System.out.println(myNum);
-
-        // final int myNum = 15;
-        // myNum = 20; // will generate an error
-        // System.out.println(myNum);
-
-//         int myNum;
-// myNum = 1000;
-// System.out.println(myNum);
-
-// int myNum = 5;
-// float myFloatNum = 5.99f;
-// char myLetter = 'D';
-// boolean myBool = true;
-// String myText = "Hello";
+        String ame = "John";
+        System.out.println("Hello " + ame);  //print variable
 
 
+        String firstName = "John ";
+        String lastName = "Doe";
+        String fullName = firstName + lastName;
+        System.out.println(fullName);
+        int showTicket = 15;
+        showTicket = 10;
+        System.out.println(showTicket);
+
+        // final int teamName = 15;
+        // teamName = 20; // will generate an error
+        // System.out.println(teamName);
+
+        int audiance;
+audiance = 1000;
+System.out.println(audiance);
+
+int Num = 5;
+float myFloatNum = 5.99f;
+char myLetter = 'D';
+boolean myBool = true;
+String myText = "Hello";
+System.out.println(Num);
+System.out.println(myFloatNum);
+System.out.println(myLetter);
+System.out.println(myBool);
+System.out.println(myText);
 int x = 5;
 int y = 6;
 int z = 50;
